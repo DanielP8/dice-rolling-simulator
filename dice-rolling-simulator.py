fad84d3ff8abf026bfd526739481630eef1466c6
DP <dp8177@proton.me>
2025-01-10 14:47:52 +0000

    Added comments and readability

--- dice-rolling-simulator.py
+++ dice-rolling-simulator.py
@@ -1,21 +1,21 @@
 # Dice Rolling Simulator
 # Room 5 - David Osei Antwi, Deirdre Boland, Nora Ishmael, Daniel Popowych Jan 2025
 
-# Import random function
+# import random function
 import random
  
-# Randomly roll dice function
-# Function randomly grabs number within range
-# Setting default values with the random roll function
+# randomly roll dice function
+# function randomly grabs number within range
+# setting default values with the random roll function
 def random_roll(min_roll=1,max_roll=6):
     dice_roll = random.randint(min_roll,max_roll)
-    #print what number is
+    # print what number is
     print(f"You rolled a {dice_roll}")
-
-# Run function
+ 
+# run function
 random_roll()
-
-# Ask if you'd like to roll again
+ 
+# ask if you'd like to roll again
 while True:
    user_input = input("Would you like to roll again? - yes/no: ").lower()
    if user_input == "yes":
